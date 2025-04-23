ea98429b7a4dcd6cbf91b49eb9fcea97e750cd6c
Anne Kroon <annekroon@gmail.com>
2025-04-23 07:03:12 +0200

    Update exercise0.py
    Added explanations to the regex

--- 6ec-course/week04/exercises/solutions/exercise0.py
+++ 6ec-course/week04/exercises/solutions/exercise0.py
@@ -1,20 +1,35 @@
-#!/usr/bin/env/python
-
 import re
 
-list_w_urls = ["some text with a url http://www.youtube.com... ",
-"and another one!! https://www.facebook.com",
-"more urls www.baidu.com??",
-"And even more?!! %$##($^) https://www.yahoo.com and this one http://www.amazon.com and this one www.wikipedia.org" ]
+list_w_urls = [
+    "some text with a url http://www.youtube.com... ",
+    "and another one!! https://www.facebook.com",
+    "more urls www.baidu.com??",
+    "And even more?!! %$##($^) https://www.yahoo.com and this one http://www.amazon.com and this one www.wikipedia.org"
+]
 
+all_urls = []
 
-# option 1: one list
-results = []
-for e in list_w_urls:
-    results.extend(re.findall(r"https?://[\w\.]+\b|www\.[\w\.]+\b", e))
+cleaned_texts = []
 
-print(results)
+for line in list_w_urls:
+    # (https?:\/\/)?   => matches 'http://' or 'https://'
+    # (www\.)?         => matches 'www.'
+    # [a-zA-Z0-9\-\.]+ => matches domain (letters, numbers, hyphens, and dots)
+    # \.[a-zA-Z]{2,}   => matches top-level domain like '.com', '.org', {2,} is a quantifier that means: Match at least 2 of the preceding character class.
+    found_urls = re.findall(r'(https?:\/\/)?(www\.)?[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}', line)
 
-# option2: nested lists
-results2 = [re.findall(r"https?://[\w\.]+\b|www\.[\w\w.]+\b", e) for e in list_w_urls]
-print(results2)
+    for url_parts in found_urls:
+        url = ''.join(url_parts)
+        all_urls.append(url)
+
+    # Remove all non-alphanumeric characters: [^a-zA-Z0-9] matches anything that is NOT a letter or number
+    cleaned_line = re.sub(r'[^a-zA-Z0-9]', '', line)
+    cleaned_texts.append(cleaned_line)
+    
+print("Extracted URLs:")
+for url in all_urls:
+    print(url)
+
+print("\nCleaned Texts:")
+for text in cleaned_texts:
+    print(text)
